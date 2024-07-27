df222100092465a551c7c93f3bbe9b161499dc23
lukassta <lukas.stavaris@gmail.com>
2024-07-27 11:05:55 +0300

    feat: Set up the start menu and settings

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,38 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-    [SerializeField] private AudioMixer myMixer;
-    [SerializeField] private Slider musicSlider;
-    [SerializeField] private Slider sfxSlider;
+    public AudioMixer myMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public TMP_Dropdown resolutionDroprown;
+
+    Resolution[] resolutions;
 
     private void Start()
     {
         LoadVolume();
+
+        resolutions = Screen.resolutions;
+
+        resolutionDroprown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x "+ resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+               resolutions[i].height == Screen.currentResolution.height)
+               {
+                    currentResolutionIndex = i;
+               }
+        }
+
+        resolutionDroprown.AddOptions(options);
+        resolutionDroprown.value = currentResolutionIndex;
+        resolutionDroprown.RefreshShownValue();
     }
 
     private void LoadVolume()
     {
-        if(PlayerPrefs.HasKey("musicVolume")) musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
-        if(PlayerPrefs.HasKey("SFXVolume")) sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
-        
-        SetMusicVolume();
-        SetSFXVolume();
+        if(PlayerPrefs.HasKey("musicVolume"))
+        {
+            float music_volume = PlayerPrefs.GetFloat("musicVolume");
+            musicSlider.value = music_volume;
+            SetMusicVolume(music_volume);
+        } 
+        if(PlayerPrefs.HasKey("SFXVolume"))
+        {
+            float SFX_volume = PlayerPrefs.GetFloat("SFXVolume");;
+            sfxSlider.value = SFX_volume;
+            SetSFXVolume(SFX_volume);
+        } 
     }
 
-    public void SetMusicVolume()
+    public void SetMusicVolume(float volume)
     {
-       float volume = musicSlider.value;
        myMixer.SetFloat("MusicVolume", Mathf.Log10(volume)*20);
        PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
-    public void SetSFXVolume()
+    public void SetSFXVolume(float volume)
     {
-        float volume = sfxSlider.value;
         myMixer.SetFloat("SFXVolume", Mathf.Log10(volume)*20);
         PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 }
